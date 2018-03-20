066dda26bece9014f498ce7a4b6dd3a5179c1248
chenliheng <1165945426@qq.com>
2018-03-20 10:10:17 +0800

    修改starter

---
+++ dubbo/spring-boot-dubbo-starter/src/main/java/com/learn/dubbo/starter/SpringBootDubboStarter.java
@@ -0,0 +1,42 @@
+package com.learn.dubbo.starter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+
+@Configuration
+public class SpringBootDubboStarter {
+
+	@Value("${spring.dubbo.applicationName:dubboApplication}")
+	private String applicationName;
+
+	@Value("${spring.dubbo.registry.type:zookeeper}")
+	private String registryType;
+
+	@Value("${spring.dubbo.registry.ip:127.0.0.1}")
+	private String registryIp;
+
+	@Value("${spring.dubbo.registry.port:2181}")
+	private String registryPort;
+
+	private static String SEPARATOR_BETWEEN_TYPE_AND_IP = "://";
+
+	private static String SEPARATOR_BETWEEN_IP_AND_PORT = ":";
+
+	@Bean
+	ApplicationConfig dubboApplicationConfig() {
+		return new ApplicationConfig(applicationName);
+	}
+
+	@Bean
+	RegistryConfig dubboRegistryConfig() {
+		StringBuilder registryUriBuilder = new StringBuilder(registryType);
+		registryUriBuilder.append(SEPARATOR_BETWEEN_TYPE_AND_IP).append(registryIp)
+				.append(SEPARATOR_BETWEEN_IP_AND_PORT).append(registryPort);
+		return new RegistryConfig(registryUriBuilder.toString());
+	}
+
+}
