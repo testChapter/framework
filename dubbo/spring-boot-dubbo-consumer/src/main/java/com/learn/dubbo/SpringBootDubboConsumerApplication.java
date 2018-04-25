ac1d2bfc33fc90bfbd9b6c0ec1ed59a3ab471fb5
chenliheng <1165945426@qq.com>
2018-04-25 16:48:19 +0800

    提交

--- dubbo/spring-boot-dubbo-consumer/src/main/java/com/learn/dubbo/SpringBootDubboConsumerApplication.java
+++ dubbo/spring-boot-dubbo-consumer/src/main/java/com/learn/dubbo/SpringBootDubboConsumerApplication.java
@@ -2,14 +2,21 @@ package com.learn.dubbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.Import;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 
 @EnableDubbo
 @SpringBootApplication
+
 public class SpringBootDubboConsumerApplication {
 
 	public static void main(String[] args) {
+		
+		//AutowiredAnnotationBeanPostProcessor
+		//CommonAnnotationBeanPostProcessor
+//		AnnotationConfigUtils.registerAnnotationConfigProcessors(registry);
 		SpringApplication.run(SpringBootDubboConsumerApplication.class, args);
 	}
 
--- dubbo/spring-boot-dubbo-provider/src/main/java/com/learn/dubbo/SpringBootDubboProviderApplication.java
+++ dubbo/spring-boot-dubbo-provider/src/main/java/com/learn/dubbo/SpringBootDubboProviderApplication.java
@@ -5,13 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
+import com.alicp.jetcache.anno.config.EnableMethodCache;
 
 @SpringBootApplication
 @EnableDubbo
 @MapperScan("com.learn.dubbo.mapper")
+@EnableMethodCache(basePackages = "com.learn.dubbo")
+@EnableCreateCacheAnnotation
 public class SpringBootDubboProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDubboProviderApplication.class, args);
 	}
+	
 }
